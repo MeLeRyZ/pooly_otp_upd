01c13a23f7ce0ed6006e1af98cf0dffa3cd4c6a1
John Weaver <meleryz@gmail.com>
2018-08-25 14:19:16 +0300

    Create all components for 3rd version of App.

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -2,12 +2,15 @@ defmodule Pooly.Supervisor do
     # the main, top-level SV
     use Supervisor
 
-    def start_link(pool_config) do
-        Supervisor.start_link(__MODULE__, pool_config)
-    end
+    def start_link(pools_config) do
+        Supervisor.start_link(__MODULE__, pools_config, name: __MODULE__)
+    end # 'name:' - adds refering to name
 
-    def init(pool_config) do
-        children = [ worker( Pooly.Server, [self, pool_config])]
+    def init(pools_config) do
+        children = [
+            supervisor( Pooly.PoolsSupervisor, [] ),
+            worker( Pooly.Server, [pools_config] )
+            ]
 
         opts = [strategy: :one_for_all]
 
--- lib/worker_supervisor.ex
+++ lib/worker_supervisor.ex
@@ -3,16 +3,18 @@ defmodule Pooly.WorkerSupervisor do
     #######
     # API #
     #######
-    def start_link({_, _, _} = mfa) do
-        Supervisor.start_link(__MODULE__, mfa)
+    def start_link(pool_server, {_, _, _} = mfa) do
+        Supervisor.start_link(__MODULE__, [pool_server, mfa])
     end
 
 
     #############
     # Callbacks #
     #############
-    def init({m, f, a} = _x) do # m - module / f - function / a - args
-        worker_opts = [restart: :permanent,
+    def init([pool_server, {m, f, a}]) do # m - module / f - function / a - args
+        Process.link(pool_server) # if one dies - second one too
+        worker_opts = [restart: :temporary,
+                        shutdown: 5000,
                         function: f]
 
         children = [worker(m, a, worker_opts)] # wr module/args/opts_to_manipulate
